62527175c80968de79e1631fce0740602297e65c
Eric Faurot <eric@faurot.net>
2025-07-08 16:09:10 +0200

    add ClosingReader and ClosingLimitedReader helpers

---
+++ reading/reading.go
@@ -0,0 +1,48 @@
+package reading
+
+import (
+	"fmt"
+	"io"
+)
+
+type closingReader struct {
+	reader   io.Reader
+	closer   io.Closer
+	isClosed bool
+}
+
+func (cr *closingReader) Read(p []byte) (int, error) {
+	if cr.isClosed {
+		return 0, io.EOF
+	}
+
+	n, err := cr.reader.Read(p)
+	if err == io.EOF {
+		// Close the file when EOF is reached
+		closeErr := cr.closer.Close()
+		cr.isClosed = true
+		if closeErr != nil {
+			return n, fmt.Errorf("error closing reader: %w", closeErr)
+		}
+	}
+	return n, err
+}
+
+func ClosingReader(reader io.ReadCloser) io.Reader {
+	return &closingReader{
+		reader:   reader,
+		closer:   reader,
+		isClosed: false,
+	}
+}
+
+func ClosingLimitedReader(reader io.ReadCloser, length int64) io.Reader {
+	return &closingReader{
+		reader: &io.LimitedReader{
+			R: reader,
+			N: length,
+		},
+		closer:   reader,
+		isClosed: false,
+	}
+}
