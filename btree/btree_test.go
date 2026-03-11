2d8cdf3ac68c0c2641c08566ac0131d4e296312b
Alizée Penel <alizee@plakar.io>
2026-03-11 10:05:01 +0100

    btree/tests: Verify btree deserialization
    Signed-off-by: Alizée Penel <alizee@plakar.io>

--- btree/btree_test.go
+++ btree/btree_test.go
@@ -1,9 +1,13 @@
 package btree_test
 
 import (
+	"bytes"
+	"io"
 	"testing"
+	"testing/iotest"
 
 	"github.com/PlakarKorp/kloset/btree"
+	"github.com/vmihailenco/msgpack/v5"
 )
 
 func cmp(a, b rune) int {
@@ -38,4 +42,69 @@ func TestFromStorage_NilStore(t *testing.T) {
 	if tree.Order != order {
 		t.Fatalf("expected order=%d, got %d", order, tree.Order)
 	}
+}
+
+func TestDeserialize(t *testing.T) {
+	t.Run("Reader Nil", func(t *testing.T) {
+		storage := btree.InMemoryStore[rune, string]{}
+
+		tree, err := btree.Deserialize(nil, &storage, cmp)
+
+		if err == nil || tree != nil {
+			t.Fatalf("expected error; tree=%v err=%v", tree, err)
+		}
+	})
+
+	t.Run("Invalid Reader", func(t *testing.T) {
+		storage := btree.InMemoryStore[rune, string]{}
+		rd := iotest.ErrReader(io.ErrUnexpectedEOF)
+
+		tree, err := btree.Deserialize[rune, int, string](rd, &storage, cmp)
+
+		if err == nil || tree != nil {
+			t.Fatalf("expected error; tree=%v err=%v", tree, err)
+		}
+	})
+
+	t.Run("Invalid Byte Encoding", func(t *testing.T) {
+		storage := btree.InMemoryStore[rune, string]{}
+
+		rd := bytes.NewReader([]byte{0x00, 0x01, 0x02})
+		tree, err := btree.Deserialize(rd, &storage, cmp)
+
+		if err == nil || tree != nil {
+			t.Fatalf("expected error; tree=%v err=%v", tree, err)
+		}
+	})
+
+	t.Run("Valid Deserialize", func(t *testing.T) {
+		storage := btree.InMemoryStore[rune, int]{}
+		payload := struct {
+			Root  int
+			Order int
+		}{
+			Root:  123,
+			Order: 3,
+		}
+
+		var buf bytes.Buffer
+		enc := msgpack.NewEncoder(&buf)
+		if err := enc.Encode(&payload); err != nil {
+			t.Fatalf("encode msgpack: %v", err)
+		}
+
+		tree, err := btree.Deserialize(&buf, &storage, cmp)
+		if err != nil {
+			t.Fatalf("expected nil err, got %v", err)
+		}
+		if tree == nil {
+			t.Fatal("expected non-nil tree")
+		}
+		if tree.Root != payload.Root {
+			t.Fatalf("expected Root=%d, got %d", payload.Root, tree.Root)
+		}
+		if tree.Order != payload.Order {
+			t.Fatalf("expected Order=%d, got %d", payload.Order, tree.Order)
+		}
+	})
 }
