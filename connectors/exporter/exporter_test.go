d9eec91ee2b29c30a6469a9de664d808af8798df
Alizée Penel <alizee@plakar.io>
2026-04-08 11:41:32 +0200

    connectors/exporter/tests: Testing Unregister
    Signed-off-by: Alizée Penel <alizee@plakar.io>

--- connectors/exporter/exporter_test.go
+++ connectors/exporter/exporter_test.go
@@ -40,3 +40,46 @@ func TestRegister(t *testing.T) {
 		require.EqualError(t, err, "exporter backend 'test-register-duplicate' already registered")
 	})
 }
+
+func TestUnregister(t *testing.T) {
+	backendFn := func(
+		ctx context.Context,
+		opts *connectors.Options,
+		proto string,
+		config map[string]string,
+	) (xport.Exporter, error) {
+		return nil, nil
+	}
+
+	t.Run("ValidBackendUnregistration", func(t *testing.T) {
+		backendName := "test-exporter-unregister-backend"
+
+		err := xport.Register(backendName, location.FLAG_LOCALFS, backendFn)
+		require.NoError(t, err)
+
+		err = xport.Unregister(backendName)
+		require.NoError(t, err)
+	})
+
+	t.Run("Fails_IfBackendIsNotRegistered", func(t *testing.T) {
+		backendName := "test-exporter-unregister-missing"
+
+		err := xport.Unregister(backendName)
+		require.EqualError(t, err, "exporter backend 'test-exporter-unregister-missing' not registered")
+	})
+
+	t.Run("Register->Unregister->Register", func(t *testing.T) {
+		backendName := "test-exporter-unregister-reregister"
+
+		err := xport.Register(backendName, location.FLAG_LOCALFS, backendFn)
+		require.NoError(t, err)
+
+		err = xport.Unregister(backendName)
+		require.NoError(t, err)
+
+		err = xport.Register(backendName, location.FLAG_LOCALFS, backendFn)
+		require.NoError(t, err)
+
+		t.Cleanup(func() { _ = xport.Unregister(backendName) })
+	})
+}
