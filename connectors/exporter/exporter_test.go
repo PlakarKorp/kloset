ba91f4d63972121e32e3b2e12e1ed2944da997cf
Alizée Penel <alizee@plakar.io>
2026-04-08 11:36:01 +0200

    connectors/exporter/tests: Testing Register
    Signed-off-by: Alizée Penel <alizee@plakar.io>

---
+++ connectors/exporter/exporter_test.go
@@ -0,0 +1,42 @@
+package exporter_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/PlakarKorp/kloset/connectors"
+	xport "github.com/PlakarKorp/kloset/connectors/exporter"
+	"github.com/PlakarKorp/kloset/location"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegister(t *testing.T) {
+	backendFn := func(
+		ctx context.Context,
+		opts *connectors.Options,
+		proto string,
+		config map[string]string,
+	) (xport.Exporter, error) {
+		return nil, nil
+	}
+
+	t.Run("ValidBackendRegistration", func(t *testing.T) {
+		backendName := "test-register-backend"
+
+		err := xport.Register(backendName, location.FLAG_LOCALFS, backendFn)
+		t.Cleanup(func() { _ = xport.Unregister(backendName) })
+		require.NoError(t, err)
+	})
+
+	t.Run("Fails_IfBackendAlreadyRegistered", func(t *testing.T) {
+		backendName := "test-register-duplicate"
+
+		err := xport.Register(backendName, location.FLAG_LOCALFS, backendFn)
+		require.NoError(t, err)
+
+		t.Cleanup(func() { _ = xport.Unregister(backendName) })
+
+		err = xport.Register(backendName, location.FLAG_LOCALFS, backendFn)
+		require.EqualError(t, err, "exporter backend 'test-register-duplicate' already registered")
+	})
+}
