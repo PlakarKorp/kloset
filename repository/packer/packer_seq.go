PlakarKorp/kloset

commit 3094bd4394ae31fb6fbf4b95e85e505783eee3f6

packer: Bubble up the error from underlying i/o error.
* We can't ignore this, especially when it's on disk temp storage.

* To ease bubbling up, and for symmetry add the last random chunk in the
  calling workers rather than in the flusher one.

repository/packer/packer_seq.go
@@ -78,8 +78,6 @@ func (mgr *seqPackerManager) Run() error {
 					continue
 				}
 
-				mgr.AddPadding(pfile, int(mgr.storageConf.Chunking.MinSize))
-
 				if err := mgr.flush(pfile); err != nil {
 					err = fmt.Errorf("failed to flush packer: %w", err)
 
@@ -121,6 +119,10 @@ func (mgr *seqPackerManager) Run() error {
 				case pm, ok := <-mgr.packerChan[i]:
 					if !ok {
 						if pfile != nil && pfile.Size() > 0 {
+							if err := mgr.AddPadding(pfile, int(mgr.storageConf.Chunking.MinSize)); err != nil {
+								return err
+							}
+
 							packerResultChan <- pfile
 						}
 						return nil
@@ -133,10 +135,14 @@ func (mgr *seqPackerManager) Run() error {
 							return err
 						}
 
-						mgr.AddPadding(pfile, int(mgr.storageConf.Chunking.MinSize))
+						if err := mgr.AddPadding(pfile, int(mgr.storageConf.Chunking.MinSize)); err != nil {
+							return err
+						}
 					}
 
-					pfile.AddBlob(pm.Type, pm.Version, pm.MAC, pm.Data, pm.Flags)
+					if err := pfile.AddBlob(pm.Type, pm.Version, pm.MAC, pm.Data, pm.Flags); err != nil {
+						return err
+					}
 
 					if pfile.Size() > mgr.storageConf.Packfile.MaxSize {
 						packerResultChan <- pfile
@@ -239,6 +245,5 @@ func (mgr *seqPackerManager) AddPadding(packfile packfile.Packfile, maxSize int)
 		return fmt.Errorf("failed to generate random padding MAC: %w", err)
 	}
 
-	packfile.AddBlob(resources.RT_RANDOM, versioning.GetCurrentVersion(resources.RT_RANDOM), mac, buffer, 0)
-	return nil
+	return packfile.AddBlob(resources.RT_RANDOM, versioning.GetCurrentVersion(resources.RT_RANDOM), mac, buffer, 0)
 }

commit ae7b7f73f2c0c33104d50466ddfeb4f3eddb3869

packer_seq: Reintroduce Last blob padding on packfiles.
* In commit 3094bd4394ae to make the code more readable I moved the end
  padding addition to the packfile construction loop (as opposed to the
  flushing), sadly I put it only in one case (the teardown phase) and
  forgot the packfile full case. Add back the padding.

Fixes: 3094bd4394ae ("packer: Bubble up the error from underlying i/o error.")

repository/packer/packer_seq.go
@@ -145,6 +145,10 @@ func (mgr *seqPackerManager) Run() error {
 					}
 
 					if pfile.Size() > mgr.storageConf.Packfile.MaxSize {
+						if err := mgr.AddPadding(pfile, int(mgr.storageConf.Chunking.MinSize)); err != nil {
+							return err
+						}
+
 						packerResultChan <- pfile
 						pfile = nil
 					}