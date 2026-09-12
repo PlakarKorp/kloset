1ae68bde1ff00239fd595169137fa81d0bae5625
Omar Polo <op@omarpolo.com>
2026-09-12 10:53:40 +0000

    connectors: add a Producer field to Record
    Integrations like k8s which run sub-integrations on their own and
    multiplex the records and results need to have a way to tell where a
    result came from.
    
    Add a "Producer" filed that's completely opaque in kloset, it's just
    echoed back in results, and it's optional, but allows integrations
    like k8s to map results back to an importer.

--- connectors/sdk.go
+++ connectors/sdk.go
@@ -36,6 +36,10 @@ type Record struct {
 	FileInfo           objects.FileInfo
 	ExtendedAttributes []string
 	FileAttributes     uint32
+
+	// kloset doesn't look at all at this field: it's only needed
+	// for consumers to map [Record]s and [Restult]s together.
+	Producer string
 }
 
 func (s *Record) Close() error {
